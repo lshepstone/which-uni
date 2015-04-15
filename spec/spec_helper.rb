e97a13c3eac71168b244f5d75aa4d51e5cac1bb3
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-04-15 11:29:14 +0100

    Adds the `bin` dir to the PATH so the command can be found

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+ENV['PATH'] = "#{File.expand_path('../../bin', __FILE__)}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
 
 require 'aruba'
 require 'aruba/api'
