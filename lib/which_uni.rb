416a8d62380b97aefec8314618b5d51b16466633
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-04-15 11:42:36 +0100

    Introduces console to return a stubbed expected output
    All green at this point! Now the tricky part ...

--- lib/which_uni.rb
+++ lib/which_uni.rb
@@ -1,4 +1,5 @@
 require 'which_uni/version'
+require 'which_uni/console'
 
 module WhichUni
   # Your code goes here...
---
+++ lib/which_uni/console.rb
@@ -0,0 +1,11 @@
+module WhichUni
+  class Console
+    def feeling_lucky_search
+      puts "which-uni version #{WhichUni::VERSION}\n"
+      puts 'Your feeling lucky search for "which university" returned the page'
+      puts '  "Which? University - find the best university & degree course"'
+      puts '  located at http://university.which.co.uk'
+      puts 'Done.'
+    end
+  end
+end
