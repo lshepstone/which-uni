0f3b79de89256db08b148bee253fa5a3e9fd45ce
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-04-15 10:36:44 +0100

    Generated a new gem project with bundler

---
+++ lib/which_uni.rb
@@ -0,0 +1,5 @@
+require "which_uni/version"
+
+module WhichUni
+  # Your code goes here...
+end
---
+++ spec/which_uni_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe WhichUni do
+  it 'has a version number' do
+    expect(WhichUni::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
