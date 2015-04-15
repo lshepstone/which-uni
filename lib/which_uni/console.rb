2580d14f1cfd84215e3c0f22fdb08e8e33a5b057
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-04-15 12:13:58 +0100

    Use the new searcher and result objects for the search
    Tests are still green at this point, though an occasional timeout is experienced (that's an Aruba thing) - we need to get some VCR in here! Now the real tricky part ...

--- lib/which_uni/console.rb
+++ lib/which_uni/console.rb
@@ -1,10 +1,13 @@
 module WhichUni
   class Console
     def feeling_lucky_search
+      query = 'which university'
+      result = Searcher.new.results_for(query).first
+
       puts "which-uni version #{WhichUni::VERSION}\n"
-      puts 'Your feeling lucky search for "which university" returned the page'
-      puts '  "Which? University - find the best university & degree course"'
-      puts '  located at http://university.which.co.uk'
+      puts "Your feeling lucky search for '#{query}' returned the page"
+      puts "  \"#{result.page_title}\""
+      puts "  located at #{result.url}"
       puts 'Done.'
     end
   end
